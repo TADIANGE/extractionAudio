2c65ef23bbb606cc50781dc317fb49bb9bacfc0c
TADIANGE <160579273+TADIANGE@users.noreply.github.com>
2024-03-01 00:21:14 +0100

    Add files via upload
    j'espere que nous avancerons

--- untitled0.py
+++ untitled0.py
@@ -1,7 +1,16 @@
 import  moviepy.editor
+#entrer le chemin de votre video
 
-video = moviepy.editor.VideoFileClip(r"codelyoko.mp4")
+chemin =input("quel est le chemin: ")
+video = moviepy.editor.VideoFileClip(chemin)
+
+#extraction de l'audio
 audio = video.audio
-audio.write_audiofile("sample.mp3")
+
+#renommage de l'audio
+nom = input("donne un nom a ton audio: ")
+audio.write_audiofile(nom)
+
+#affichage que tout c'est bien passer
 print("you are audio was extracted succefully")
 
